e71795bfa83f08e542ff5a4e482f0860e91bd5f6
fk368 <fk368@FAZLU>
2023-10-05 14:29:55 +0530

    Add project files.

---
+++ arrayDemo/Laptop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayDemo
+{
+    internal class Laptop
+    {
+        int _ram;
+            public int Ram { get { return _ram;} set{_ram =value ;} }
+
+        private decimal _price;
+
+        public decimal Price
+        {
+            get 
+            { 
+            return _price; 
+            }
+            set 
+            { 
+            _price = value;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Laptop hp = new Laptop();
+            hp.Ram = 100;
+            Console.WriteLine(hp.Ram);
+
+            Console.ReadLine();
+        }
+    }
+}
